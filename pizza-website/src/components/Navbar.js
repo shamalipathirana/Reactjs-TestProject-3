2f13d5df79cc643be3109f825731ca1094aacbdd
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-16 07:56:15 +0530

    Add logo to Navbar

---
+++ pizza-website/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../assets/Logo.png'
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="leftSide">
+            <img src={Logo}></img>
+            </div>
+            <div className="righttSide">
+
+            </div>
+
+        </div>
+
+    )
+}
+
